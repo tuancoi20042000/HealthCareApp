7c57716afa3d6312c84be5d93761b0b3c8c9ccd3
github_tuancoi20042000 <chmlys14@gmail.com.vn>
2021-10-24 10:41:56 +0700

    edit cart, list order xml

--- app/src/main/java/com/example/healthcareapp/OrderDetailActivity.java
+++ app/src/main/java/com/example/healthcareapp/OrderDetailActivity.java
@@ -2,13 +2,7 @@ package com.example.healthcareapp;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
 
 public class OrderDetailActivity extends AppCompatActivity {
 
@@ -16,16 +10,7 @@ public class OrderDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_detail);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 }
